13524e17381a9a90ad610b7ba16b2d8e16e2ff1c
rhqudco <rhqudco1204@naver.com>
2022-07-08 20:58:48 +0900

    MemberRepositoy, MemberService

--- src/main/java/bear/blog/repository/MemberRepository.java
+++ src/main/java/bear/blog/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,5 +15,21 @@ public class MemberRepository {
 
     public void save(Member member) {
         em.persist(member);
+    }
+
+    public Member findOne(Long no) {
+        return em.find(Member.class, no);
+    }
+
+    public List<Member> findById(String id) {
+        return em.createQuery("Select m From Member m where m.id =:id", Member.class)
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    public List<Member> findByNickname(String nickname) {
+        return em.createQuery("Select m From Member m where m.nickname =:nickname", Member.class)
+                .setParameter("nickname", nickname)
+                .getResultList();
     }
 }
