2cf5b858c700e46260e8f63aeb9e953e5f6c26ad
rhqudco <rhqudco1204@naver.com>
2022-07-19 23:48:26 +0900

    PW Encrypt Test Complete Real

--- src/test/java/bear/blog/common/WebSecurityConfigTest.java
+++ src/test/java/bear/blog/common/WebSecurityConfigTest.java
@@ -33,9 +33,10 @@ public class WebSecurityConfigTest {
         // given
         String rawPW = "1234";
         String encodePW = webSecurityConfig.getPasswordEncoder().encode(rawPW);
+        String inputPW = "1234";
 
         // when
-        Boolean check = webSecurityConfig.getPasswordEncoder().matches(rawPW, encodePW);
+        Boolean check = webSecurityConfig.getPasswordEncoder().matches(inputPW, encodePW);
 
         // then
         assertThat(check).isEqualTo(true);
@@ -45,19 +46,15 @@ public class WebSecurityConfigTest {
     @DisplayName("패스워드 불일치 테스트")
     void notMatchTest() {
         // given
-        String originalPW = "1234";
-        String mistakePW = "12345678";
-        String encodeFakePW = webSecurityConfig.getPasswordEncoder().encode(mistakePW);
+        String originalPW = "1234"; // 설정한 PW의 원본
+        String inputPW = "123456"; // 입력한 PW의 원본
+        String encodePW = webSecurityConfig.getPasswordEncoder().encode(originalPW); // 설정한 PW를 인코딩한 값
 
         // when
-        Boolean check = webSecurityConfig.getPasswordEncoder().matches(originalPW, encodeFakePW);
+        Boolean check = webSecurityConfig.getPasswordEncoder().matches(inputPW, encodePW); // 입력한 PW와 설정한 PW를 인코딩한 값을 matches()를 통해 비교한다.
+        // originalPW와 inputPW가 다르기 때문에 false 나와야 함.
 
         // then
         assertThat(check).isEqualTo(false);
-
-        // print
-        System.out.println("originalPW = " + originalPW);
-        System.out.println("mistakePW = " + mistakePW);
-        System.out.println("encodeFakePW = " + encodeFakePW);
     }
 }
